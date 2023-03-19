21c2a86cb19cdbb5b9180f2c3be80ece13db9076
gab-villa <aquilesbaesa340@gmail.com>
2023-03-18 23:16:09 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+/*Main file*/
+"use strict";
+
+//import {CircularQueue} from './modules/CircularQueue.js';
+//import {Global} from './modules/Global.js';
+import {setOptionsOfQuestion} from './modules/OptionsOfQuestion.js'
+import {MAXLENGTHWENG, MAXLENGTHMAIN, MAXQUANTQUESTIONS, IDWORD, WENG, WSPA} 
+       from './modules/Definitions.js';
+import {getRandomInt, getRandomIntFromInterval} from './modules/RandFunctions.js'
+import {wordQueue} from './modules/WordQueue.js';
+import {mainQueue} from './modules/MainQueue.js'
+
+//var mainQueue = new CircularQueue(MAXLENGTHMAIN);
+//var wengQueue = new CircularQueue(MAXLENGTHWENG);
+//var ulWordList = document.getElementById("wordList");
+//var ul = createWengList();
+
+/*
+for(let i = 0;i < MAXLENGTHWENG;i++)
+{
+  appendChildToWengList("nue");
+}*/
+
+
+//const myRequest = new Request("listaTraducidaEngSpaArr.json");
+fetch("resources/wordLists/listaTraducidaEngSpaArrA12B12.json")
+.then(res => res.json())
+.then(data => {
+      let dataRef = { 
+          data : data.slice()
+      };
+      
+      // take in account array limit and index
+      
+      mainQueue.chargeRandomNumbersToMainQueue(dataRef);
+      wordQueue.chargeRandomNumbersToQueue();
+      //const firstOption = wordQueue.q.getFront();
+
+      //console.log(firstWord.idword);
+      //take a random word from the left and right extreme
+      //let wordsInSpa = createOptions(dataRef, firstOption[IDWORD] -1);
+      //console.log(wordsInSpa);
+      //console.log(data[numWrong1][WSPA]);
+      
+      setOptionsOfQuestion();
+      //setGlobalValue(0,wengQueue);
+});
+
+function mostrar(weng)
+{
+  console.log(weng[WENG] + " " + weng[WSPA]);
+}
+
+/*
+var readJson = (path, cb) => {
+  fs.readFile(require.resolve(path), (err, data) => {
+    if (err)
+      cb(err)
+    else
+      cb(null, JSON.parse(data))
+  })
+}*/
---
+++ modules/CircularQueue.js
@@ -0,0 +1,42 @@
+// circular queue
+export {CircularQueue};
+
+class CircularQueue {
+ constructor(size) {
+  this.element = [];
+  this.size = size;
+  this.length = 0;
+  this.front = 0;
+  this.back = -1;
+ }
+isEmpty() {
+  return this.length == 0;
+ }
+isFull() {
+  return this.length >= this.size;
+}
+enqueue(element) {
+  if (this.isFull()) throw (new Error("Maximum length exceeded"));
+  this.back++;
+   this.element[this.back % this.size] = element;
+  this.length++;
+ }
+dequeue() {
+  if (this.isEmpty()) throw (new Error("No elements in the queue"));
+  const value = this.getFront();
+  this.element[this.front % this.size] = null;
+  this.front++;
+  this.length--;
+  return value;
+ }
+getFront() {
+  if (this.isEmpty()) throw (new Error("No elements in the queue"));
+  return this.element[this.front % this.size];
+ }
+clear() {
+  this.element = new Array();
+  this.length = 0;
+  this.back = 0;
+  this.front = -1;
+ }
+}
